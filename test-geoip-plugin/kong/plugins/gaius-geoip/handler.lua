3d8bab20d2d7b56753f27b5cd32c32b4d7641d75
Ant-Ghost <aunomitra.ghosh1999@gmail.com>
2024-05-15 10:43:03 +0530

    made few changes in handler.lua

--- test-geoip-plugin/kong/plugins/gaius-geoip/handler.lua
+++ test-geoip-plugin/kong/plugins/gaius-geoip/handler.lua
@@ -35,6 +35,16 @@ end
 --- full :{"city":{"geoname_id":1799962,"names":{"en":"Nanjing","ru":"Нанкин","fr":"Nankin","pt-BR":"Nanquim","zh-CN":"南京","es":"Nankín","de":"Nanjing","ja":"南京市"}},"subdivisions":[{"geoname_id":1806260,"names":{"en":"Jiangsu","fr":"Province de Jiangsu","zh-CN":"江苏省"},"iso_code":"32"}],"country":{"geoname_id":1814991,"names":{"en":"China","ru":"Китай","fr":"Chine","pt-BR":"China","zh-CN":"中国","es":"China","de":"China","ja":"中国"},"iso_code":"CN"},"registered_country":{"geoname_id":1814991,"names":{"en":"China","ru":"Китай","fr":"Chine","pt-BR":"China","zh-CN":"中国","es":"China","de":"China","ja":"中国"},"iso_code":"CN"},"location":{"time_zone":"Asia\/Shanghai","longitude":118.7778,"accuracy_radius":50,"latitude":32.0617},"continent":{"geoname_id":6255147,"names":{"en":"Asia","ru":"Азия","fr":"Asie","pt-BR":"Ásia","zh-CN":"亚洲","es":"Asia","de":"Asien","ja":"アジア"},"code":"AS"}}
 ---
 
+-- LOOPS
+function includeOrExcludes(arr, element, initial_value)
+  for i,line in ipairs(arr) do
+    if line == element then
+      return (initial_value + 1)%2
+    end 
+  end
+  return initial_value
+end
+
 -- Access Phase
 function GaiusGeoIP:access(conf)
   geo.init(MMDB_PATH)
@@ -46,11 +56,7 @@ function GaiusGeoIP:access(conf)
   end
 
   -- Global ip restriction allowed
-  if kong.ctx.shared.is_trusted then
-    return
-  end
-
-  if remote_addr == "127.0.0.1" then
+  if kong.ctx.shared.is_trusted and remote_addr == "127.0.0.1" then
     return
   end
   
@@ -68,32 +74,24 @@ function GaiusGeoIP:access(conf)
   end
   
   -- BLOCK IP IF MATCH RULES
-  local block = 0
-  if ( conf.mode == "Blacklist" and conf.blacklist_countries ~= nil ) then 
-    for i,line in ipairs(conf.blacklist_countries) do
-      if line == country.iso_code then
-        block = 1
-      end 
-    end
-  elseif ( conf.mode == "Whitelist" ) then
-    block = 1
-    if conf.whitelist_countries ~= nil then
-      for i,line in ipairs(conf.whitelist_countries) do
-        if line == country.iso_code then
-          block = 0
-        end
-      end
+  local block = 1
+
+  -- PRIORTIZING WHITELISTED IPS
+  if conf.whitelist_ips and #conf.whitelist_ips > 0 then
+    local allowed = match_bin(conf.whitelist_ips, binary_remote_addr)
+    if allowed then
+      block = 0
     end
   end
 
   if block == 1 then
-    if conf.whitelist_ips and #conf.whitelist_ips > 0 then
-      local allowed = match_bin(conf.whitelist_ips, binary_remote_addr)
-      if allowed then
-        block = 0
-      end
+    if ( conf.mode == "Blacklist" and conf.blacklist_countries ~= nil ) then
+      block = includeOrExcludes(conf.blacklist_countries, country.iso_code, 0)
+    elseif ( conf.mode == "Whitelist" and conf.whitelist_countries ~= nil) then
+      block = includeOrExcludes(conf.blacklist_countries, country.iso_code, 1)
     end
   end
+
   
   
   if block == 1 then
